20b988192b41b9202dd07a05ce978cfd39b46fc4
Banderi <banderi002@gmail.com>
2021-05-29 15:49:19 +0200

    Fixed skull icon for a plague

--- src/core/table_translation.h
+++ src/core/table_translation.h
@@ -173,7 +173,7 @@ static int groupid_translation_table_ph[] = {
         209, 9 + 341, // delivery boy
         210, 45 + 66, // bazaar (fancy)
         213, 17 + 66, // senet house
-        227, 293, // data/Pharaoh_General.sg3 Sprites.bmp
+        227, 163, // skull
         228, 180 + 341, // doctor (apothecary)
 //        229, 229+341, // patrician
 //        230, 230+341, // missionary
