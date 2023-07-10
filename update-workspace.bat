43c03790aac87e64d607012b2a706cc3c0164b03
dalerank <dalerankn8@gmail.com>
2023-07-10 23:14:28 +0200

    Create ozymandias.yml

--- update-workspace.bat
+++ update-workspace.bat
@@ -1,5 +1,13 @@
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ../
-start ozymandias.sln /D .
+set "build_type=%1"
+set "debug_flag="
+if "%1"=="" (
+  set "build_type=Debug"
+  set "debug_flag=/D"
+)
+
+cmake -DCMAKE_BUILD_TYPE=%build_type% ../
+start ozymandias.sln %debug_flag% .
+cd ..
 
