a5fb7787a2125a166dbb0e5efb5e72373c60b40b
s.kushnirenko <s.kushnirenko@gaijin.ru>
2023-07-16 18:03:57 +0200

    rename struct difficulty_data_t to g_difficulty_data for clarity

--- src/game/difficulty.cpp
+++ src/game/difficulty.cpp
@@ -3,12 +3,14 @@
 #include "core/calc.h"
 #include "game/settings.h"
 
-static const struct {
+struct difficulty_data_t {
     int money;
     int enemies;
     int starting_kingdom;
     int sentiment;
-} data[] = {
+};
+
+static const difficulty_data_t g_difficulty_data[] = {
         {300, 40,  70, 80}, // very easy
         {200, 60,  60, 70}, // easy
         {150, 80,  50, 60}, // normal
@@ -17,19 +19,19 @@ static const struct {
 };
 
 int difficulty_starting_kingdom(void) {
-    return data[setting_difficulty()].starting_kingdom;
+    return g_difficulty_data[setting_difficulty()].starting_kingdom;
 }
 
 int difficulty_sentiment(void) {
-    return data[setting_difficulty()].sentiment;
+    return g_difficulty_data[setting_difficulty()].sentiment;
 }
 
 int difficulty_adjust_money(int money) {
-    return calc_adjust_with_percentage(money, data[setting_difficulty()].money);
+    return calc_adjust_with_percentage(money, g_difficulty_data[setting_difficulty()].money);
 }
 
 int difficulty_adjust_enemies(int enemies) {
-    return calc_adjust_with_percentage(enemies, data[setting_difficulty()].enemies);
+    return calc_adjust_with_percentage(enemies, g_difficulty_data[setting_difficulty()].enemies);
 }
 
 int difficulty_adjust_wolf_attack(int attack) {
