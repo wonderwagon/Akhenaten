390a3e39793fca41347a29237232a218d49a4d15
Artem Kuskis <artem.kuskis@gmail.com>
2023-08-18 22:57:29 +0100

    Make dependency on tracy sources conditional

--- src/core/profiler.h
+++ src/core/profiler.h
@@ -1,9 +1,23 @@
 #pragma once
 
+#ifndef TRACY_ENABLE
+
+#define OZZY_PROFILER_BEGIN
+#define OZZY_PROFILER_FRAME(x)
+#define OZZY_PROFILER_SECTION(x)
+#define OZZY_PROFILER_TAG(y, x)
+#define OZZY_PROFILER_LOG(text, size)
+#define OZZY_PROFILER_VALUE(text, value)
+
+#else
+
 #include "tracy/Tracy.hpp"
+
 #define OZZY_PROFILER_BEGIN ZoneScoped
 #define OZZY_PROFILER_FRAME(x) FrameMark
 #define OZZY_PROFILER_SECTION(x) ZoneScopedN(x)
 #define OZZY_PROFILER_TAG(y, x) ZoneText(x, strlen(x))
 #define OZZY_PROFILER_LOG(text, size) TracyMessage(text, size)
 #define OZZY_PROFILER_VALUE(text, value) TracyPlot(text, value)
+
+#endif
