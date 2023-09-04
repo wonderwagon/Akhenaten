7c6cec1eee0840a2ca0b32532b2f33ac2ec1b002
s.kushnirenko <s.kushnirenko@gaijin.ru>
2023-09-05 00:45:31 +0200

    solution: use relwithdebuginfo mode when build exe

--- update-workspace-linux.sh
+++ update-workspace-linux.sh
@@ -3,5 +3,5 @@ sudo apt -qq update
 sudo apt install --yes cmake build-essential libsdl2-dev libsdl2-mixer-dev libsdl2-image-dev
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 cd ..
