675c4639bb786fc39c96ff3dc8857770b683d45c
s.kushnirenko <s.kushnirenko@gaijin.ru>
2023-09-18 01:52:25 +0200

    solution: fix: #120: properly compilation for clang

--- src/resource/icons.cpp
+++ src/resource/icons.cpp
@@ -1,6 +1,23 @@
 #include "icons.h"
+#include "platform/platform.h"
+
+#if defined(GAME_PLATFORM_WIN)
+#pragma warning( push )
+#pragma warning( disable : 4838 )
+#pragma warning( disable : 4309 )
+#else
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnarrowing"
+#endif
 
 #include "discord_icon.h"
+
+#if defined(GAME_PLATFORM_WIN)
+#pragma warning ( pop )
+#else
+#pragma clang diagnostic pop
+#endif
+
 #include "platform/screen.h"
 #include "platform/renderer.h"
 
