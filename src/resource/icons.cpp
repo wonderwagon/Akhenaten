d57590afa265fa5daa989dab4db8438df85037a9
s.kushnirenko <s.kushnirenko@gaijin.ru>
2023-09-25 00:21:00 +0200

    solution: pack app icon/js script to resources

--- src/resource/icons.cpp
+++ src/resource/icons.cpp
@@ -11,7 +11,6 @@
 #endif
 
 #include "discord_icon.h"
-#include "app_icon.h"
 
 #if defined(GAME_PLATFORM_WIN)
 #pragma warning ( pop )
@@ -21,30 +20,50 @@
 
 #include "platform/screen.h"
 #include "platform/renderer.h"
+#include "cmrc/cmrc.hpp"
+
+CMRC_DECLARE(ozymandias);
 
 #include <SDL.h>
-
 #include <string.h>
+#include <assert.h>
 
 SDL_Surface *IMG_LoadPNG_RW(SDL_RWops *src);
+constexpr pcstr internal_res_path = "res/";
+
+std::pair<void *, uint32_t> internal_read_data(pcstr path) {
+    if (!path || !*path) {
+        return {nullptr, 0};
+    }
+
+    auto fs = cmrc::ozymandias::get_filesystem();
+
+    bstring256 fs_path(internal_res_path, (*path == ':') ? (path + 1) : path);
+    if (!fs.exists(fs_path.c_str())) {
+        return {nullptr, 0};
+    }
+
+    auto fd1 = fs.open(fs_path.c_str());
+    return {(void*)fd1.begin(), (uint32_t)fd1.size()};
+}
 
 SDL_Texture *load_icon_texture(const char *name) {
     SDL_Surface *loadedSurface = nullptr;
     if (strcmp(name, "discord") == 0) {
         return graphics_renderer()->create_texture_from_png_buffer((void*)gDiscordIcon, gDiscordIcon_length);
-    } else if (strcmp(name, "app") == 0) {
-        return graphics_renderer()->create_texture_from_png_buffer((void*)gAppIcon, gAppIcon_length);
     }
-
-    return nullptr;
+    
+    auto data = internal_read_data(name);
+    return graphics_renderer()->create_texture_from_png_buffer((void*)data.first, data.second);
 }
 
 SDL_Surface *load_icon_surface(const char *name) {
     SDL_RWops *rw = nullptr;
     if (strcmp(name, "discord") == 0) {
         rw = SDL_RWFromMem((void*)gDiscordIcon, gDiscordIcon_length);
-    } else if (strcmp(name, "app") == 0) {
-        rw = SDL_RWFromMem((void*)gAppIcon, gAppIcon_length);
+    } else {
+        auto data = internal_read_data(name);
+        rw = SDL_RWFromMem((void *)data.first, data.second);
     }
 
     if (rw) {
---
+++ src/scripts/scripts.cpp
@@ -0,0 +1,27 @@
+#include "content/content.h"
+
+#include "cmrc/cmrc.hpp"
+
+CMRC_DECLARE(ozymandias);
+
+namespace vfs {
+
+constexpr pcstr internal_scripts_path = "src/scripts/";
+
+std::pair<void*, uint32_t> internal_file_open(pcstr path) {
+	if (!path || !*path) {
+		return {nullptr, 0};
+	}
+
+	auto fs = cmrc::ozymandias::get_filesystem();
+
+	bstring256 fs_path(internal_scripts_path, (*path == ':') ? (path + 1) : path);
+	if (!fs.exists(fs_path.c_str())) {
+		return {nullptr, 0};
+	}
+
+	auto fd1 = fs.open(fs_path.c_str());
+	return {(void*)fd1.begin(), (uint32_t)fd1.size()};
+}
+
+}
