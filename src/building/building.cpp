423f48dcd88c01a5ad4214aec314a9406f208fea
s.kushnirenko <s.kushnirenko@gaijin.ru>
2023-10-08 23:17:51 +0200

    ui: show overlay button for firehouse

--- src/building/building.cpp
+++ src/building/building.cpp
@@ -426,8 +426,8 @@ e_overlay building::get_overlay() const {
         case BUILDING_TEMPLE_BAST:
             return OVERLAY_RELIGION_BAST;
 
-        case BUILDING_ENGINEERS_POST:
-            return OVERLAY_DAMAGE;
+        case BUILDING_ENGINEERS_POST: return OVERLAY_DAMAGE;
+        case BUILDING_FIREHOUSE: return OVERLAY_FIRE;
     }
 
     return OVERLAY_NONE;
