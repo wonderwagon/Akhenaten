fffc0723d4bfaf6d5b6c4dff844f2f9c955a6ac5
s.kushnirenko <s.kushnirenko@gaijin.ru>
2024-05-27 01:51:22 +0200

    fixed compilation for linux

--- src/building/building_military_academy.h
+++ src/building/building_military_academy.h
@@ -13,5 +13,5 @@ public:
     virtual void update_count() const override;
     virtual e_overlay get_overlay() const override { return OVERLAY_CRIME; }
     virtual e_sound_channel_city sound_channel() const override { return SOUND_CHANNEL_CITY_POLICE; }
-    virtual bool draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) override;
+    //virtual bool draw_ornaments_and_animations_height(painter &ctx, vec2i point, tile2i tile, color color_mask) override;
 };
