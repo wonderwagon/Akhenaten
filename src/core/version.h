c1e5e44f7715a23deb5d94286d4030adc88623f9
s.kushnirenko <s.kushnirenko@gaijin.ru>
2024-06-28 10:28:25 +0200

    bump minor version number

--- src/core/version.h
+++ src/core/version.h
@@ -2,8 +2,8 @@
 
 #define GAME_VERSION_MAJOR 0
 #define GAME_VERSION_MINOR 2
-#define GAME_VERSION_REVSN 1
+#define GAME_VERSION_REVSN 2
 
 #ifndef GAME_BUILD_NUMBER
 #define GAME_BUILD_NUMBER 0xff
-#endif
+#endif // GAME_BUILD_NUMBER
